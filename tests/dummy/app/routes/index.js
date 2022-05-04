418a189b871b2f58ebb5f9050cfce64d1dae4eba
Nick Steffens <nsteffens@movableink.com>
2022-05-04 11:15:05 -0400

    fix: removed redirect

--- tests/dummy/app/routes/index.js
+++ tests/dummy/app/routes/index.js
@@ -1,9 +1,3 @@
 import Route from '@ember/routing/route';
-import { inject as service } from '@ember/service';
 
-export default class DummyIndexRoute extends Route {
-  @service router;
-  redirect() {
-    this.router.replaceWith('docs');
-  }
-}
+export default class DummyIndexRoute extends Route {}
